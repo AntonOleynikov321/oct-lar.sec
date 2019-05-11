b2dacf3069963e8e391c7a3fe1b52ccca74fcd46
introqt <introtheintro@gmail.com>
2019-05-11 09:53:51 +0300

    correct edit tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,37 +3,48 @@
 use Illuminate\Http\Request;
 use App\Task;
 
-Route::get('/', function () {
+Route::get('/tasks', function () {
     $tasks = Task::all();
     return view('tasks.index',[
         'tasks'=>$tasks,
         ]); // в уроке это вид tasks
-});
+})->name('tasks_index');
 Route::post('/tasks', function (Request $request) {
     $validator = Validator::make($request->all(), [
                 'name' => 'required|max:255',
     ]);
     if ($validator->fails()) {
-        return redirect('/')
+        return redirect(route('tasks_index'))
                         ->withInput()
                         ->withErrors($validator);
     }
     $task = new Task();
     $task->name = $request->name;
     $task->save();
-    return redirect('/');
-});
+    return redirect(route('tasks_index'));
+})->name('tasks_store');
+
 Route::delete('/tasks/{task}',function(Task $task){
     $task->delete();
-    return redirect('/');
-});
+    return redirect(route('tasks_index'));
+})->name('tasks_destroy');
+
 Route::get("/tasks/{task}/edit",function(Task $task){
     return view('tasks.edit',[
         'task'=>$task,
         ]);
-});
+})->name('tasks_edit');
+
 Route::put("tasks/{task}",function(Request $request, Task $task){
+     $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect(route('tasks_edit', $task->id))
+                        ->withInput()
+                        ->withErrors($validator);
+    }
     $task->name = $request->name;
     $task->save();
-    return redirect('/');
-});
+    return redirect(route('tasks_index'));
+})->name('tasks_update');
